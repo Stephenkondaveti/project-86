4bfd3d7cfe5dbf28e6cedaa3ca142723cf483ef7
Stephenkondaveti <73046071+Stephenkondaveti@users.noreply.github.com>
2021-02-07 17:11:44 -0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,46 @@
+var canvas = new fabric.Canvas('myCanvas')
+
+block_image_width = 30;
+block_image_height = 30;
+
+player_x = 10;
+player_y = 10;
+
+var player_object = "";
+var player_object1 = "";
+
+function player_update()
+{
+    fabric.Image.fromURL("player.png", function(Img)
+    {
+        player_object = Img;
+
+        player_object.scaleToWidth(150);
+        player_object.scaleToHeight(140);
+        player_object.set({
+            top:player_y,
+            left:player_x
+        });
+        canvas.add(player_object);
+
+    });
+
+}
+
+function new_image(get_image)
+{
+    fabric.Image.fromURL(get_image, function(Img)
+    {
+        block_image_object = Img;
+
+        player_object.scaleToWidth(block_image_width);
+        player_object.scaleToHeight(block_image_height);
+            player_object.set({
+            top:player_y,
+            left:player_x
+        });
+        canvas.add(block_image_object);
+
+    });
+
+}
